b52545ca4c12cba4dd09ea030bfe2cc07868b133
kouki <kouworld0123@gmail.com>
2018-05-09 14:07:18 +0900

    作った

---
+++ clipboard_to_translate.rb
@@ -0,0 +1,15 @@
+require 'easy_translate'
+require 'dotenv'
+
+# get text from clipboard
+text = `pbpaste`
+puts text
+# remove ¥n
+text.gsub!(/(\r\n|\r|\n)/, " ")
+puts text
+# throw google translate
+Dotenv.load
+text = EasyTranslate.translate(text, to: :japanese, key: ENV["GOOGLE_API_KEY"])
+# result to clipboard
+# system "pbcopy", "\"#{text}\""
+IO.popen('pbcopy', 'w') { |f| f << text }
