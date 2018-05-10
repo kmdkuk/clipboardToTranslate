7a0872777933eb1ff38b22a8ff15124e435d234a
kouki <kouworld0123@gmail.com>
2018-05-10 15:25:22 +0900

    module化

--- clipboard_to_translate.rb
+++ clipboard_to_translate.rb
@@ -1,16 +1,28 @@
 require 'easy_translate'
 require 'dotenv'
 
-# get text from clipboard
-text = `pbpaste`
-puts text
-# remove ¥n
-text.gsub!(/(\r\n|\r|\n)/, " ")
-puts text
-# throw google translate
-Dotenv.load
-text = EasyTranslate.translate(text, to: :japanese, key: ENV["GOOGLE_API_KEY"])
-puts text
-# result to clipboard
-# system "pbcopy", "\"#{text}\""
-IO.popen('pbcopy', 'w') { |f| f << text }
+module ClipboardToTranslate
+  class CLI
+    attr_reader :argv
+
+    def initialize(argv = [])
+      @argv = argv.dup
+    end
+
+    def run
+      # get text from clipboard
+      text = `pbpaste`
+      puts text
+      # remove ¥n
+      text.gsub!(/(\r\n|\r|\n)/, " ")
+      puts text
+      # throw google translate
+      Dotenv.load
+      text = EasyTranslate.translate(text, to: :japanese, key: ENV["GOOGLE_API_KEY"])
+      puts text
+      # result to clipboard
+      # system "pbcopy", "\"#{text}\""
+      IO.popen('pbcopy', 'w') { |f| f << text }
+    end
+  end
+end
